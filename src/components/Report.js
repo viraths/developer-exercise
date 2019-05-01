6213c0d91334c10163bb4f137585da6a1e57170c
Pradeep Senevirathne <pradeep.senevirathne@ladbrokes.com.au>
2019-05-01 15:43:16 +1000

    Add backend api

--- src/components/Report.js
+++ src/components/Report.js
@@ -1,9 +1,59 @@
 import React from 'react';
+import { API_ENDPOINT } from '../constants';
 
 class Report extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            populationGrowth: {
+                country: '',
+                averageGrowth: ''
+            },
+            co2Emission: {
+                year: '',
+                highestEmission: ''
+              }
+        };
+    }
+    getReportData() {
+        fetch('/report')
+            .then(res => res.json())
+            .then(res => this.setState(res));
+    }
+
+    componentWillMount() {
+        this.getReportData();
+    }
+
     render() {
         return (
-            <div>WIP</div>
+            <div>
+                <h1>ABX - Developer exercis (getting there)</h1>
+                <h3>The country with the higest average of "Urban population growth (annual %)" between 1980 and 1990</h3>
+                <table>
+                    <tbody>
+                        <tr><td colSpan="2">The country with the higest average of "Urban population growth (annual %)" between 1980 and 1990</td></tr>
+                        <tr>
+                            <td>Country:</td>
+                            <td>{this.state.populationGrowth.country}</td>
+                        </tr>
+                        <tr>
+                            <td>Average:</td>
+                            <td>{this.state.populationGrowth.averageGrowth}</td>
+                        </tr>
+                        <tr><td colSpan="2"></td></tr>
+                        <tr><td colSpan="2">The year with the highest "CO2 emissions (kt)"</td></tr>
+                        <tr>
+                            <td>Country:</td>
+                            <td>{this.state.co2Emission.year}</td>
+                        </tr>
+                        <tr>
+                            <td>Average:</td>
+                            <td>{this.state.co2Emission.highestEmission}</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
         );
     }
 }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './containers/App';
 
 ReactDOM.render(
-  <h1>ABX - Developer exercis (getting there)</h1>,
+  <App />,
   document.getElementById('root')
 );
